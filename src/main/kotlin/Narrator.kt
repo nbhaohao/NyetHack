27531955bf8603a34c14a471a85861b0a2cf12cd
nbhaohao <nbhaohao@163.com>
2023-10-01 16:01:09 +0800

    feat: lambda syntax

---
+++ src/main/kotlin/Narrator.kt
@@ -0,0 +1,8 @@
+val narrationModifier: (String) -> String = { message ->
+    val numExclamationPoints = 3
+    message.uppercase() + "!".repeat(numExclamationPoints)
+}
+
+fun narrate(message: String) {
+    println(narrationModifier(message))
+}
