010750edd3cfe35975dde2f3c89ada3ede1aae6e
nbhaohao <nbhaohao@163.com>
2023-10-04 15:27:06 +0800

    use map

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-private const val TAVERN_MASTER = "Taerny1"
+private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
 
 private val firstNames = setOf("Alex", "Mordoc", "Sophie", "Tariq")
@@ -21,17 +21,35 @@ fun visitTavern() {
     narrate(menuItems.joinToString())
 
     val patrons: MutableSet<String> = mutableSetOf()
-    repeat(10) {
-        patrons += "${firstNames.random()} ${lastNames.random()}"
+    val patronGold = mutableMapOf(
+        TAVERN_MASTER to 86.00,
+        heroName to 4.50
+    )
+    while (patrons.size < 5) {
+        val patronName = "${firstNames.random()} ${lastNames.random()}"
+        patrons += patronName
+        patronGold += patronName to 6.0
     }
     narrate("$heroName sees several patrons in the tavern:")
     narrate(patrons.joinToString())
+
+    println(patronGold)
     repeat(3) {
-        placeOrder(patrons.random(), menuItems.random())
+        placeOrder(patrons.random(), menuItems.random(), patronGold)
     }
+    println(patronGold)
 }
 
-private fun placeOrder(patronName: String, menuItemName: String) {
+private fun placeOrder(patronName: String, menuItemName: String, patronGold: MutableMap<String, Double>) {
+    val itemPrice = 1.0
+
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
-    narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+    if (itemPrice <= patronGold.getOrDefault(patronName, 0.0)) {
+        narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+        narrate("$patronName pays $TAVERN_MASTER $itemPrice gold")
+        patronGold[patronName] = patronGold.getValue(patronName) - itemPrice
+        patronGold[TAVERN_MASTER] = patronGold.getValue(TAVERN_MASTER) + itemPrice
+    } else {
+        narrate("$TAVERN_MASTER says, \"You need more coin for a $menuItemName\"")
+    }
 }
