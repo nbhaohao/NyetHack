49adc1876aaf0b03450b17583926155cfc133735
nbhaohao <nbhaohao@163.com>
2023-10-05 10:37:39 +0800

    scope function

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -42,12 +42,12 @@ fun visitTavern() {
     }
     displayPatronBalances(patronGold)
 
-    val departingPatrons: List<String> = patrons.filter { patron ->
+    patrons.filter { patron ->
         patronGold.getOrDefault(patron, 0.0) < 4.0
-    }
-    patrons -= departingPatrons
-    patronGold -= departingPatrons
-    departingPatrons.forEach { patron ->
+    }.also { departingPatrons ->
+        patrons -= departingPatrons
+        patronGold -= departingPatrons
+    }.forEach { patron ->
         narrate("$heroName sees $patron departing the tavern")
     }
     narrate("There are still some patrons in the tavern")
